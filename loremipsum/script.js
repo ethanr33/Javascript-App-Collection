23713be694ed0c5e576f147aac414275348510f2
ethanr33 <35584218+ethanr33@users.noreply.github.com>
2019-10-12 07:56:38 -0700

    Added functionality and finished mobile website
    Added the main app functionality and finished the responsive mobile version of the website for the Lorem Ipsum project

--- loremipsum/script.js
+++ loremipsum/script.js
@@ -1,13 +1,48 @@
 
-const loremIpsum = require("lorem-ipsum").loremIpsum;
-
 window.onload = () => {
 
   const generateLoremButton = document.getElementById("generate-text");
   const loremTextContainer = document.getElementById("generated-text");
+  const numParagraphs = document.getElementById("num-paragraphs");
+
+  const corsProxy = "https://cors-anywhere.herokuapp.com/";
+
+  let isActivated = false;
 
   generateLoremButton.addEventListener("click", () => {
-    console.log(loremIpsum());
+    let input = numParagraphs.value;
+    if (isNaN(input)) {
+        loremTextContainer.innerHTML = "Please enter a number";
+    } else if (input <= 0) {
+        loremTextContainer.innerHTML = "Please enter a positive number";
+    } else if (Math.floor(input) != input) {
+        loremTextContainer.innerHTML = "Please enter a whole number";
+    } else {
+      loremTextContainer.innerHTML = `Generating ${input} paragraphs...`;
+      getLoremText(input, (text, response) => {
+        loremTextContainer.innerHTML = text;
+      })
+    }
   });
+
+  async function getLoremText(paragraphs, callback) {
+    let requestHeaders = {
+      method: "GET",
+      headers: {
+        "Content-Type": "text/plain"
+      }
+    }
+
+     let data = await fetch(`${corsProxy}http://loripsum.net/api/${paragraphs}/medium/plaintext`, requestHeaders)
+      .then(response => response.text())
+      .catch(error => {
+          callback("There was an error, check the console", response);
+      })
+      .then(text => {
+          callback(text);
+      })
+
+      return data;
+  }
 
 }
