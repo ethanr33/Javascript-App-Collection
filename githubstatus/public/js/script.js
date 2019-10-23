37923c4742563e65f55dedc4a33612129a744a20
ethanr33 <35584218+ethanr33@users.noreply.github.com>
2019-10-23 05:55:26 -0700

    Added responsiveness to Github Status
    Added some responsiveness to the Github Status project. Updated font size and table size based on browser width and height.

--- githubstatus/public/js/script.js
+++ githubstatus/public/js/script.js
@@ -8,15 +8,36 @@ window.onload = () => {
   const gistsStatus = document.getElementById("gists-status");
   const pagesStatus = document.getElementById("pages-status");
   
-  getGithubStatuses(data => {
-    githubStatus.innerHTML += ` ${data[0]}`;
-    apiStatus.innerHTML += ` ${data[1]}`;
-    issuesStatus.innerHTML += ` ${data[2]}`;
-    notificationsStatus.innerHTML += ` ${data[3]}`;
-    gistsStatus.innerHTML += ` ${data[4]}`;
-    pagesStatus.innerHTML += ` ${data[5]}`;
-
+  const refreshStatusButton = document.getElementById("refresh-status-button");
+  
+  
+  refreshStatusButton.addEventListener("click", () => {
+    setGithubStatuses();
   });
+  
+  setGithubStatuses();
+  
+  
+  function setFetchingStatuses() {
+    githubStatus.innerHTML = "Fetching...";
+    apiStatus.innerHTML = "Fetching...";
+    issuesStatus.innerHTML = "Fetching...";
+    notificationsStatus.innerHTML = "Fetching...";
+    gistsStatus.innerHTML = "Fetching...";
+    pagesStatus.innerHTML = "Fetching...";
+  }
+  
+  function setGithubStatuses() {
+    setFetchingStatuses();
+    getGithubStatuses(data => {
+      githubStatus.innerHTML = data[0];
+      apiStatus.innerHTML = data[1];
+      issuesStatus.innerHTML = data[2];
+      notificationsStatus.innerHTML = data[3];
+      gistsStatus.innerHTML = data[4];
+      pagesStatus.innerHTML = data[5];
+    });
+  }
   
   function getGithubStatuses(callback) {
     fetch("/status")
